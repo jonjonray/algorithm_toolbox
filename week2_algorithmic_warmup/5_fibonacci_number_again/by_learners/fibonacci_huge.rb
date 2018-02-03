9dde4ef631835fa0526fea45c29cd79b4c0b89f6
jonjonray <jonjonray@gmail.com>
2018-02-02 19:08:13 -0500

    compllete pisano period problem

--- week2_algorithmic_warmup/5_fibonacci_number_again/by_learners/fibonacci_huge.rb
+++ week2_algorithmic_warmup/5_fibonacci_number_again/by_learners/fibonacci_huge.rb
@@ -1,8 +1,32 @@
 #!/usr/bin/env ruby
 # by Andronik Ordian
+def pisano_period(m)
+  i = 0
+  a = 0
+  b = 1
+  c = a + b
+  while i < m * m
+    c = (a + b) % m
+    a = b
+    b = c
+
+    return i + 1 if a == 0 && b == 1
+    i+= 1
+  end
+end
 
 def fib_huge(n, m)
-  
+  remainder = n % pisano_period(m)
+
+  fibonacci_nums = [0,1]
+
+  i = 2
+  while i <= remainder
+    fibonacci_nums.push(fibonacci_nums[-1] + fibonacci_nums[-2])
+    i+= 1
+  end
+
+  fibonacci_nums[-1] % m
 end
 
 if __FILE__ == $0
