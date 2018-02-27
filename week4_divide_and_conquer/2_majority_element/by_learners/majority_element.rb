632977d125673c9bb2e9bac6316cdbdc76085ed2
jonjonray <jonjonray@gmail.com>
2018-02-26 17:04:41 -0800

    complete 3/4 week 4

--- week4_divide_and_conquer/2_majority_element/by_learners/majority_element.rb
+++ week4_divide_and_conquer/2_majority_element/by_learners/majority_element.rb
@@ -5,7 +5,20 @@ def get_majority_element(a, left, right)
   return -1 if left >= right
   return a[left] if right - left == 1
 
-  
+  left_recur = get_majority_element(a, left, (left - right) / 2)
+  right_recur = get_majority_element(a, ((left - right) / 2) + 1, right)
+
+  if left_recur == right_recur
+    return left_recur
+  elsif left_recur != -1 && right_recur == -1
+    a[(left - right) / 2 + 1..right].each do |int|
+      return left_recur if int == left_recur
+    end
+  elsif right_recur != -1 && left_recur == -1
+    a[left..(left - right) / 2].each do |int|
+      return right_recur if int == right_recur
+    end
+  end
   -1
 end
 
